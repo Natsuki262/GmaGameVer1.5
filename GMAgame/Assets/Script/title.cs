0d3b16f21fc44666e90184cf700420654d02467e
akiraaaaa610 <a5.k7.r0.9ja3ga1i4mo@docomo.ne.jp>
2018-10-11 00:33:46 +0900

    [add]タイトルとリザルトのシーン遷移スクリプトを追加しました

---
+++ GMAgame/Assets/Script/title.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour {
+
+    bool MouseButton = false;          //マウスをクリックしたか
+
+    public AudioClip SE_Start;         //スタートの音
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //左クリックがされたら
+        if (Input.GetMouseButtonDown(0) && MouseButton == false)
+        {
+            MouseButton = true;
+            //BGMを止める
+            GetComponent<AudioSource>().Stop();
+            //決定音を鳴らす
+            GetComponent<AudioSource>().PlayOneShot(SE_Start);
+            //GameStart関数を呼び出す
+            StartCoroutine("GameStart");
+        }
+    }
+
+    //少し待ってシーン遷移
+    IEnumerator GameStart()
+    {
+        // 1.5秒待つ
+        yield return new WaitForSeconds(1.5f); 
+        //シーンの移動
+        SceneManager.LoadScene("MainScene");
+    }
+}
