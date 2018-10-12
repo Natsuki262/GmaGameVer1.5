0eea33882c72d1c98c0f472a06e7770f8eecfdb5
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-10-12 16:12:04 +0900

    【add】TitleManagerを追加Maingameに遷移

---
+++ GMAgame/Assets/Script/TitleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager: MonoBehaviour {
+
+     //スタートの音
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        
+    }
+
+    //少し待ってシーン遷移
+    IEnumerator GameStart()
+    {
+        // 1.5秒待つ
+        yield return new WaitForSeconds(1.5f); 
+       
+    }
+   public void PushStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+       
+    }
+}
