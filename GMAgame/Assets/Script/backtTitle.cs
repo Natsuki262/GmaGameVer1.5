0a02f6076815626975374a0f04a44d4430f5e305
Okui_Hirotoshi <hiro.58.fgl.2525.952@icloud.com>
2018-10-30 01:25:15 +0900

    ｛add】backTitlescriptを追加しました。

---
+++ GMAgame/Assets/Script/backtTitle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backtTitle : MonoBehaviour
+{
+
+    //スタートの音
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    //少し待ってシーン遷移
+    IEnumerator GameStart()
+    {
+        // 1.5秒待つ
+        yield return new WaitForSeconds(1.5f);
+
+    }
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("Title");
+
+    }
+}
